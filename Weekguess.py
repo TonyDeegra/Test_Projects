342b2c29081fe9dcc43d531e1559fccf6ae31393
Ilya <155441682+TonyDeegra@users.noreply.github.com>
2024-01-03 02:13:12 +0600

    Add files via upload

---
+++ Weekguess.py
@@ -0,0 +1,17 @@
+week_day = int(input('Введите номер для недели: '))
+if week_day == 1:
+    print('Понедельник')
+elif week_day == 2:
+    print('Вторник')
+elif week_day == 3:
+    print('Среда')
+elif week_day == 4:
+    print('Четверг')
+elif week_day == 5:
+    print('Пятница')
+elif week_day == 6:
+    print('Суббота')
+elif week_day == 7:
+    print('Воскресенье')
+else:
+    print('Введите число от 1 до 7')
