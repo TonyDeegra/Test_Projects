75e4c0c1cff4d5422c6f25f8def104f450621c93
Ilya <155441682+TonyDeegra@users.noreply.github.com>
2024-01-07 04:29:41 +0600

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,8 @@
+login = input('Login: ')
+password = input('Password: ')
+your_login = input('Your login: ')
+your_password = input('Your Password: ')
+if login == your_login and your_password == password:
+    print("Welcome!")
+else:
+    print('You shall not pass!!!')
